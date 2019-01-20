c47944739e9a822fd9a67627fa23d6a1e6502cf8
dwyanwede <shang_sk@163.com>
2019-01-20 12:49:30 +0800

    java基础Instance

---
+++ src/main/java/com/learn/demo/java/base/TestInstance.java
@@ -0,0 +1,39 @@
+package com.learn.demo.java.base;
+
+class A {
+}
+
+class B extends A {
+}
+
+public class TestInstance {
+    public static void main(String[] args) {
+
+        B b = new B();
+        A a = new A();
+        A ba = new B();
+        System.out.println("1------------");
+        System.out.println(b instanceof B);
+        System.out.println(b instanceof A);
+        System.out.println(b instanceof Object);
+        System.out.println(null instanceof Object);
+        System.out.println("2------------");
+        System.out.println(b.getClass().isInstance(b));
+        System.out.println(b.getClass().isInstance(a));
+        System.out.println("3------------");
+        System.out.println(a.getClass().isInstance(ba));
+        System.out.println(b.getClass().isInstance(ba));
+        System.out.println(b.getClass().isInstance(null));
+        System.out.println("4------------");
+        System.out.println(A.class.isInstance(a));
+        System.out.println(A.class.isInstance(b));
+        System.out.println(A.class.isInstance(ba));
+        System.out.println("5------------");
+        System.out.println(B.class.isInstance(a));
+        System.out.println(B.class.isInstance(b));
+        System.out.println(B.class.isInstance(ba));
+        System.out.println("6------------");
+        System.out.println(Object.class.isInstance(b));
+        System.out.println(Object.class.isInstance(null));
+    }
+}
