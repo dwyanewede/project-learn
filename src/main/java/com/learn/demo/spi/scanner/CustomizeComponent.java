ed47cc24a703446e0fe5e08f2021d09b9406d051
dwyanwede <shang_sk@163.com>
2019-02-12 09:41:00 +0800

    自定义Component

---
+++ src/main/java/com/learn/demo/spi/scanner/CustomizeComponent.java
@@ -0,0 +1,20 @@
+package com.learn.demo.spi.scanner;
+
+import org.springframework.stereotype.Indexed;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Indexed
+public @interface CustomizeComponent {
+
+	/**
+	 * The value may indicate a suggestion for a logical component name,
+	 * to be turned into a Spring bean in case of an autodetected component.
+	 * @return the suggested component name, if any (or empty String otherwise)
+	 */
+	String value() default "";
+
+}
