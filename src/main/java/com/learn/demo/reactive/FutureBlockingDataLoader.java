85e0ac6877ce4a2e8944a73abe1be56ff37ecd18
dwyanwede <shang_sk@163.com>
2019-02-14 14:38:14 +0800

    并发编程-future

---
+++ src/main/java/com/learn/demo/reactive/FutureBlockingDataLoader.java
@@ -0,0 +1,26 @@
+package com.learn.demo.reactive;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureBlockingDataLoader extends DataLoader {
+    protected void doLoad() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3); // 创建线程池
+        runCompletely(executorService.submit(super::loadConfigurations));
+        runCompletely(executorService.submit(super::loadUsers));
+        runCompletely(executorService.submit(super::loadOrders));
+        executorService.shutdown();
+    }
+
+    private void runCompletely(Future<?> future) {
+        try {
+            future.get();
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new FutureBlockingDataLoader().load();
+    }
+}
