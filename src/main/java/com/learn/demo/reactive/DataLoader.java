105d3a23ff1ffd5ee785643afbdaec009f3eaa28
dwyanwede <shang_sk@163.com>
2019-02-14 14:36:36 +0800

    同步编程-load

---
+++ src/main/java/com/learn/demo/reactive/DataLoader.java
@@ -0,0 +1,55 @@
+package com.learn.demo.reactive;
+
+import java.util.concurrent.TimeUnit;
+/**
+ * @ClassName: ExecutorCompletionServiceManager
+ * @Description: CompletionService管理线程池处理任务的返回结果
+ * @Author: 尚先生
+ * @CreateDate: 2019/02/12 14:42
+ * @Version: 1.0
+ */
+public class DataLoader {
+
+    public final void load() {
+        long startTime = System.currentTimeMillis(); // 开始时间
+        doLoad(); // 具体执行
+        long costTime = System.currentTimeMillis() - startTime; // 消耗时间
+        System.out.println("load() 总耗时：" + costTime + " 毫秒");
+    }
+
+    protected void doLoad() { // 串行计算
+        loadConfigurations();    //  耗时 1s
+        loadUsers();                  //  耗时 2s
+        loadOrders();                // 耗时 3s
+    } // 总耗时 1s + 2s  + 3s  = 6s
+
+    protected final void loadConfigurations() {
+        loadMock("loadConfigurations()", 1);
+    }
+
+    protected final void loadUsers() {
+        loadMock("loadUsers()", 2);
+    }
+
+    protected final void loadOrders() {
+        loadMock("loadOrders()", 3);
+    }
+
+    private void loadMock(String source, int seconds) {
+        try {
+            long startTime = System.currentTimeMillis();
+            long milliseconds = TimeUnit.SECONDS.toMillis(seconds);
+            Thread.sleep(milliseconds);
+            long costTime = System.currentTimeMillis() - startTime;
+            System.out.printf("[线程 : %s] %s 耗时 :  %d 毫秒\n",
+                    Thread.currentThread().getName(), source, costTime);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new DataLoader().load();
+    }
+
+}
