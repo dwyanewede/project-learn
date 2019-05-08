b66485c82ac77be300efac1b3b9f875dfd9fc1c2
dwyanwede <shang_sk@163.com>
2019-05-08 11:23:50 +0800

    test for customer

---
+++ src/main/java/com/learn/demo/spi/scanner/ScanClass.java
@@ -0,0 +1,12 @@
+package com.learn.demo.spi.scanner;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScanClass {
+
+	public void print() {
+		System.out.println("scanClass1");
+	}
+
+}
