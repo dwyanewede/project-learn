925b7a79072ccbe0e21fc2012dd382aae4d75ec0
dwyanwede <shang_sk@163.com>
2019-05-09 13:11:37 +0800

    线程中断

--- src/main/java/com/learn/demo/concurrent/xiaomage/thread/ThreadStateQuestion.java
+++ src/main/java/com/learn/demo/concurrent/xiaomage/thread/ThreadStateQuestion.java
@@ -1,17 +1,28 @@
 package com.learn.demo.concurrent.xiaomage.thread;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadStateQuestion {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // main 线程 -> 子线程
         Thread thread = new Thread(() -> { // new Runnable(){ public void run(){...}};
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.printf("线程[%s] 正在执行...\n", Thread.currentThread().getName());  // 2
         }, "子线程-1");
 
         // 启动线程
         thread.start();
+
+        // 若调用 sleep()而使线程处于 TIMED-WATING 状态，这时调用 interrupt()方法，会抛出
+        //InterruptedException,
+        thread.interrupt();
 
         // 先于 Runnable 执行
         System.out.printf("线程[%s] 是否还活着: %s\n", thread.getName(), thread.isAlive()); // 1
