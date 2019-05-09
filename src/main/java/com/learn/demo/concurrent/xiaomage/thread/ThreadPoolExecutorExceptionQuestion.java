c84ba8b0020954f39381920dd73d18b1ab77018f
dwyanwede <shang_sk@163.com>
2019-05-09 10:56:47 +0800

    ThreadPoolExecutor 回调和拒绝策略使用

--- src/main/java/com/learn/demo/concurrent/xiaomage/thread/ThreadPoolExecutorExceptionQuestion.java
+++ src/main/java/com/learn/demo/concurrent/xiaomage/thread/ThreadPoolExecutorExceptionQuestion.java
@@ -13,26 +13,34 @@ public class ThreadPoolExecutorExceptionQuestion {
                 1,
                 0,
                 TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>()
-        ) {
+                // TODO 1. 设置队列长度可使用默认的拒绝策略，否则会无限扩展
+                // Exception in thread "main" java.util.concurrent.RejectedExecutionException: Task com.learn.demo.concurrent.xiaomage.thread
+                new LinkedBlockingQueue<>(2)
+        )
+                // TODO 2. 实现回调
+//        {
+//
+//            /**
+//             * 通过覆盖 {@link ThreadPoolExecutor#afterExecute(Runnable, Throwable)} 达到获取异常的信息
+//             * @param r
+//             * @param t
+//             */
+//            @Override
+//            protected void afterExecute(Runnable r, Throwable t) {
+//                System.out.printf("线程[%s] 遇到了异常，详细信息：%s\n",
+//                        Thread.currentThread().getName(),
+//                        t.getMessage());
+//            }
+//
+//        }
+ ;
 
-            /**
-             * 通过覆盖 {@link ThreadPoolExecutor#afterExecute(Runnable, Throwable)} 达到获取异常的信息
-             * @param r
-             * @param t
-             */
-            @Override
-            protected void afterExecute(Runnable r, Throwable t) {
-                System.out.printf("线程[%s] 遇到了异常，详细信息：%s\n",
-                        Thread.currentThread().getName(),
-                        t.getMessage());
-            }
-
-        };
-
-        executorService.execute(() -> {
-            throw new RuntimeException("数据达到阈值");
-        });
+        for(int i =0; i < 10; i++){
+            executorService.execute(() -> {
+                // TODO 2. 实现回调
+                // throw new RuntimeException("数据达到阈值");
+            });
+        }
 
         // 等待一秒钟，确保提交的任务完成
         executorService.awaitTermination(1, TimeUnit.SECONDS);
