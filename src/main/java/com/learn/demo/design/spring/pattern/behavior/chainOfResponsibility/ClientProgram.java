5672e06b266f11f0ab81502c72fa839e3137c4a1
dwyanwede <shang_sk@163.com>
2019-05-10 09:54:34 +0800

    责任链模式可以作为流程系统的简单实现 -待扩展

--- src/main/java/com/learn/demo/design/spring/pattern/behavior/chainOfResponsibility/ClientProgram.java
+++ src/main/java/com/learn/demo/design/spring/pattern/behavior/chainOfResponsibility/ClientProgram.java
@@ -4,6 +4,10 @@ package com.learn.demo.design.spring.pattern.behavior.chainOfResponsibility;
 public class ClientProgram {
 
 	public static void main(String[] args) {
+		// TODO 责任链模式可以作为流程系统的简单实现
+		// 感觉此种实现存在问题，由于流程流转到下一个节点，可以直接在当前节点选择下一节点角色信息
+		// 可以根据发起人所对应的 parentId去向上找对应的经理跟HR，此处需要设计字段 parentId（一对一）
+		// 如果需要实现多用户扩展，可以查询可选列表，供当前节点选择指派，此处需要设计两个字段 id 和 parentId（一对多）
 		TeamLeader tl = new TeamLeader();
 		ProjectLeader pl = new ProjectLeader();
 		HR hr = new HR();
