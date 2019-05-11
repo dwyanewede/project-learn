550faa1b9d603ddee09d78b07cc65eb0964437c4
dwyanwede <shang_sk@163.com>
2019-05-11 10:44:47 +0800

    多产品接口设计，配置文件读取设计

--- src/main/java/com/learn/demo/interfaces/loan/product/common/BeanNameUtils.java
+++ src/main/java/com/learn/demo/interfaces/loan/product/common/BeanNameUtils.java
@@ -1,13 +1,41 @@
 package com.learn.demo.interfaces.loan.product.common;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Properties;
+
 /**
  * @ClassName: BeanNameUtils
- * @Description: BeanName 获取工具类
+ * @Description: BeanName 获取工具类， 从配置文件中读取对应的 BeanName
  * @Author: 尚先生
  * @CreateDate: 2019/5/10 16:46
  * @Version: 1.0
  */
-public enum  BeanNameUtils {
+@Component
+@PropertySource(value = "classpath:interfaces/pattern/app-product.properties")
+public class BeanNameUtils {
 
+    @Autowired
+    private Environment environment;
+
+    public String getBeanName(String transCode, String productCode, String channelId){
+        // 由于是局部变量，不存在线程安全问题，故此采用 StringBuilder
+        StringBuilder key = new StringBuilder();
+        key.append(transCode).append("-" + productCode).append("-" + channelId);
+        String value = environment.getProperty(key.toString());
+        System.out.println("获取的BeanName为： " + value);
+        return value;
+    }
 
 }
